ad5faec19915b45b24bbcdb0ee69589cc049554f
g00dy22 <g00dy22@mt2014.com>
2019-08-06 21:55:54 +0300

    more cleanup cases, iframes as links

--- informator_wrapper.user.js
+++ informator_wrapper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         informator wrapper
 // @namespace    dev/null
-// @version      0.4.3
+// @version      0.4.4
 // @description  try to take over the world!
 // @author       g00dy22
 // @include      /^https?:\/\/[^\.]*\.?informator\.ua\/.*$/
@@ -10,6 +10,10 @@
 
 (function() {
     'use strict';
+    function prepare_query(q) {
+        return document.evaluate(q, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    }
+
     function remove_nodes(q) {
         for (var i = 0; i < q.snapshotLength; i++) {
             q.snapshotItem(i).remove();
@@ -25,8 +29,35 @@
             q.snapshotItem(i).parentNode.parentNode.remove();
         }
     }
-    function prepare_query(q) {
-        return document.evaluate(q, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    function extractVideoIframe(q) {
+        for (var i = 0; i < q.snapshotLength; i++) {
+          var elem = q.snapshotItem(i);
+          for (var j = 0; j < elem.children.length; j++) {
+            var c = elem.children.item(j);
+            if (c.nodeName == 'IFRAME') {
+              var replacementNode = document.createElement("a")
+              replacementNode.setAttribute("href", c.src);
+              replacementNode.appendChild(document.createTextNode(c.src))
+              c.replaceWith(replacementNode);
+            }
+          }
+          elem.setAttribute("id", "content-area");
+          elem.setAttribute("class", "post type-post status-publish hentry");
+        }
+    }
+    function extractVideoDiv(q) {
+        for (var i = 0; i < q.snapshotLength; i++) {
+          var elem = q.snapshotItem(i);
+          for (var j = 0; j < elem.children.length; j++) {
+            elem.children.item(j).remove();
+          }
+          var replacementNode = document.createElement("a")
+          replacementNode.setAttribute("href", elem.getAttribute("data-href"));
+          replacementNode.appendChild(document.createTextNode(elem.getAttribute("data-href")))
+
+          elem.appendChild(replacementNode)
+          elem.setAttribute("class", "aligncenter");
+        }
     }
     function contains_condition(w) {
         return 'contains(#ENTITY#,"' + w + '")';
@@ -49,11 +80,11 @@
         }
 
         // Remove articles by keywords
-        var t_ckeywords = ["КаВуН", "советы эксперта", "сегодня праздник", "футбол",
+        var t_ckeywords = ["КаВуН", "КавуН", "советы эксперта", "сегодня праздник", "футбол", ": какой сегодня день",
                            "Сравнение цен", ": ТОП ", "ТОП-10", "Кредит", "рассрочка", "черная пятница",
-                          "кинопремьер", "куда пойти", "Информатор FM", "Мамахохотала", "Зеленский",
-                          "Как сэкономить", "Конкорд", "итоги недели", "Загид", "SOHO", "Stand Up",
-                          "Днепр в лицах", "Тимошенко", "кредит", "бойко", "Что подарить", "Concord"
+                           "кинопремьер", "куда пойти", "Информатор FM", "Мамахохотала", "Зеленский",
+                           "Как сэкономить", "Конкорд", "итоги недели", "Загид", "SOHO", "Stand Up",
+                           "Днепр в лицах", "Тимошенко", "кредит", "бойко", "Что подарить", "Concord"
         ]
         var t_skeywords = ["ТОП"]
 
@@ -66,10 +97,29 @@
         remove_parent_nodes(prepare_query('//li[@class="infinite-post"]/a[' + title_expression.replace(/#ENTITY#/g, '@title') + ']'));
 
         // Remove paragraphs by keywords
-        var p_keywords = ["Ранее мы","Ранее Информатор","быть в курсе всех событий","SOHO","Хотите узнавать первым","Напомним, наши коллеги"]
+        var p_keywords = ["Ранее мы","Ранее Информатор","быть в курсе всех","SOHO","Хотите узнавать первым",
+                          "Напомним, наши коллеги", "Если вы стали свидетелем", "Если вы хотите знать",
+                          "Напомним, мы показывали", "Мы продолжим сообщать вам", "если вы стали свидетелем",
+                          "Также писали о том, что", "если вы хотите знать", "107,3", "почитают память преподоб"
+        ]
 
-        var paragraph_expression = p_keywords.map(contains_condition).join(' or ')
+        // Remove paragraphs by start keywords
+        var p_skeywords = ["Ранее сообщалось, что", "Напомним, что", "Вы можете посмотреть,", "Авто Информатор рассказывал,",
+                           "Мы сообщали о том, что", "Ранее Информатор"
+        ]
+
+        var paragraph_expression = p_keywords.map(contains_condition).join(' or ') + ' or ' + p_skeywords.map(starts_condition).join(' or ')
         remove_nodes(prepare_query('//*[@id="content-main"]/p[' + text_entity(paragraph_expression) + ']'));
+        remove_nodes(prepare_query('//*[contains(@class, "post-info-right")]/p[' + text_entity(paragraph_expression) + ']'));
+        remove_nodes(prepare_query('//*[@id="content-main"]/*/p[' + text_entity(paragraph_expression) + ']'));
+        remove_nodes(prepare_query('//*[contains(@class, "post-info-right")]/*/p[' + text_entity(paragraph_expression) + ']'));
+        remove_parent_nodes(prepare_query('//*[@id="content-main"]/p/span[' + text_entity(paragraph_expression) + ']'));
+
+        // Replace youtube iframes with clickable links
+        extractVideoIframe(prepare_query('//div[@id="video-embed"]'));
+        // Replace fb divs with clickable links
+        extractVideoIframe(prepare_query('//div[@class="video-container"]'));
+        extractVideoDiv(prepare_query('//div[@class="fb-video"]'));
     }
 
     window.setInterval(page_ads, 1000);
